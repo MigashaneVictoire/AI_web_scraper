95cb90a2a020c09bb7f83eea8b5b7281073fb67b
migashanevictoire <migashanevictorie@gmail.com>
2025-02-15 11:44:13 -0500

    complet parser file and interace

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 # imports
 import streamlit as st
-from scrape import scrape_site
+from scrape import *
+from parser_ml import parse_with_ollama
 
 st.title("Web Scraper AI") # add a title
 url = st.text_input("Enter a site URL") # text input box
 
 if st.button("Fetch"):
     st.write("scraping the site ... (DO NOT refresh site)") # writen after button pressed
+
     result = scrape_site(url)
-    print(result)
+    body_content = body_content_extraction(result)
+    cleaned_cont = clean_content(body_content)
 
+    st.session_state.dom_content = cleaned_cont
 
+    with st.expander("View DOM content"):
+        st.text_area("DOM content", cleaned_cont, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you are trying to parse?")
+
+    if st.button("Parse Content"):
+        if  parse_description:
+            st.write("Parsing the content!")
+
+            dom_chunks = separate_dom_content(st.session_state.dom_content)
+            parse_result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(parse_result)
--- scrape.py
+++ scrape.py
@@ -1,20 +1,23 @@
+from bs4 import BeautifulSoup
 from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 from selenium.webdriver.common.by import By
 import credentials as cred
 import time
+from bs4 import BeautifulSoup
 
+# bright data credentials
 AUTH = cred.authentication()
 SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
 
 # Setup selenium chrome driver
 def scrape_site(website):
     print('Connecting to Scraping Browser...')
-
+    # brightdata set up
     sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
     with Remote(sbr_connection, options=ChromeOptions()) as driver:
         print('Connected! Navigating...')
-        driver.get(website)
+        driver.get(website) #lunch website
 
         # CAPTION Handling
         print("waiting caption to solve...")
@@ -26,5 +29,51 @@ def scrape_site(website):
         # print('Captcha solve status:', solve_cap['value']['status'])
         print('Navigated! Scrapping page content...')
         html = driver.page_source
-        time.sleep(10)
+        time.sleep(2)
         return html
+
+
+def body_content_extraction(html_trafic):
+    soup = BeautifulSoup(html_trafic, "html.parser")
+    body_html = soup.body
+    if body_html:
+        return str(body_html)
+    else:
+        return ""
+
+# clean the html content
+def clean_content(body_html):
+    soup = BeautifulSoup(body_html, "html.parser")
+
+    # remove script and style contents
+    for script_or_style in soup(["script","style"]):
+        script_or_style.extract()
+
+    # remove empty text strings | remove \n and leading and trailing spaces
+    cleaned_content = soup.get_text(separator= "\n")
+    cleaned_content= "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+    return cleaned_content
+
+# deal with the token limit
+def separate_dom_content(dom_content, max_len=6000):
+    # create bashes of 6000 elements in length
+    # grap the first 6000 elements and loop starting at the 6001 and count another 6000 elements...
+    # until it grabs all the elements on the page
+    return [
+        dom_content[i:i - max_len] for i in range(0, len(dom_content), max_len)
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
